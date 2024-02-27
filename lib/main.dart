9ed26f0389e6b1e76b9ed45df23001c6e3dd5bcf
hashirSoomro <hashirsomroo@gmail.com>
2024-02-27 15:56:13 +0500

    Connect Firebase
    Connect Firebase with flutter project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'screens/auth-ui/sign-in-screen.dart';
 import 'screens/auth-ui/sign-up-screen.dart';
 import 'screens/auth-ui/splash-screen.dart';
 import 'screens/user-panel/main-screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
