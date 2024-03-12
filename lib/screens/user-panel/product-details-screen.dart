61ad4d800a063b8878d0aee2bec7e24994d1da18
hashirSoomro <hashirsomroo@gmail.com>
2024-03-12 16:16:32 +0500

    Create Add Products Method
    Create Add Products Method using Firebase

--- lib/screens/user-panel/product-details-screen.dart
+++ lib/screens/user-panel/product-details-screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_comm/models/cart-model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -185,5 +187,51 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   }
 
   //check product exist or not
-  Future<void> checkProductExistence({required String uId}) async {}
+  Future<void> checkProductExistence({
+    required String uId,
+    int quantityIncrement = 1,
+  }) async {
+    final DocumentReference documentReference = FirebaseFirestore.instance
+        .collection('cart')
+        .doc(uId)
+        .collection('cartOrders')
+        .doc(widget.productModel.productId.toString());
+
+    DocumentSnapshot snapshot = await documentReference.get();
+
+    if (snapshot.exists) {
+      int currentQuantity = snapshot['productQuantity'];
+      int updatedQuantity = currentQuantity + quantityIncrement;
+      double totalPrice =
+          double.parse(widget.productModel.fullPrice) * updatedQuantity;
+
+      await documentReference.update({
+        'productQuantity': updatedQuantity,
+        'productTotalPrice': totalPrice
+      });
+    } else {
+      await FirebaseFirestore.instance.collection('cart').doc(uId).set({
+        'uId': uId,
+        'createdAt': DateTime.now(),
+      });
+
+      CartModel cartModel = CartModel(
+          productId: widget.productModel.productId,
+          categoryId: widget.productModel.categoryId,
+          productName: widget.productModel.productName,
+          categoryName: widget.productModel.categoryName,
+          salePrice: widget.productModel.salePrice,
+          fullPrice: widget.productModel.fullPrice,
+          productImages: widget.productModel.productImages,
+          deliveryTime: widget.productModel.deliveryTime,
+          isSale: widget.productModel.isSale,
+          productDescription: widget.productModel.productDescription,
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+          productQuantity: 1,
+          productTotalPrice: double.parse(widget.productModel.fullPrice));
+
+      await documentReference.set(cartModel.toMap());
+    }
+  }
 }
